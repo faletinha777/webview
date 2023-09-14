201aa2b9b8707e2ef4a794aaf2be05435b6ef421
renan <faletarenan2@gmail.com>
2023-09-13 22:16:24 -0300

    Initial commit

---
+++ app/src/main/java/com/renan/testen1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.renan.testen1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("168.138.224.182:8085/TerminalWeb/Login.aspx?ReturnUrl=%2fTerminalWeb%2f"); // URL do site que você deseja incorporar
+    }
+}
